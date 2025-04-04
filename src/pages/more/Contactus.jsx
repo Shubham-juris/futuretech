cc14c41b38fa08da540c736f8b24d6a58a6d3dcb
dheerajjuris <dheeraj.juris@gmail.com>
2025-04-04 16:04:16 +0530

    contact us page created

--- src/pages/more/Contactus.jsx
+++ src/pages/more/Contactus.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Form from '../../Components/common/Form';
-
+import WorkingHours from '../../Components/WorkingHours';
+import Map from '../../Components/Map'
 const Contactus = () => {
   return (
     <>
       <Form />
+      <WorkingHours/>
+      <Map/>
       
     </>
   )
