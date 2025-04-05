8e82d768264b784e7a9a9fa0cf20f0f80fa3c197
roshanjuris <roshan.juris@gmail.com>
2025-04-05 14:53:12 +0530

    worked on image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-
+import itService from "../assets/itService/itservice.jpg";
+import marketing from "../assets/marketing/marketing.jpg";
+import developments from "../assets/developments/developments.jpg";
 const Home = () => {
   return (
     <>
-      {" "}
-      <div className="relative w-full h-screen flex items-center justify-center bg-cover bg-center"
-        style={{
-          backgroundImage:
-            "url(' https://cdn.pixabay.com/photo/2019/07/14/16/27/pen-4337521_1280.jpg')",
-        }}
+      <div
+        className="relative w-full h-screen flex items-center justify-center 
+  bg-[url('https://cdn.pixabay.com/photo/2019/07/14/16/27/pen-4337521_1280.jpg')]
+   bg-cover bg-center"
       >
-        <div className="absolute inset-0 bg-pink bg-opacity-50"></div>
-
         <div className="relative text-center text-white px-6">
           <h1 className="text-4xl md:text-6xl font-bold drop-shadow-lg">
             Professional Tech For <br /> Your Business
@@ -21,12 +19,15 @@ const Home = () => {
           </p>
         </div>
       </div>
+      <h1 className="text-4xl font-bold text-gray-700 text-center py-10">
+        Services
+      </h1>
       <div className="max-w-5xl mx-auto px-4 py-10">
-        <div className="flex flex-col md:flex-row items-center justify-center gap-8">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-8 ">
           <img
-            src="https://images.freeimages.com/images/premium/previews/1227/12275386-development-graph.jpg"
+            src={itService}
             alt="Development"
-            className="w-full md:w-1/2 rounded-lg shadow-lg"
+            className="w-full md:w-2/5 shadow-lg"
           />
           <div className="text-center md:text-left">
             <h2 className="text-xl font-semibold text-center"> IT Services</h2>
@@ -41,7 +42,7 @@ const Home = () => {
             </p>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-center gap-8">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-8 py-20">
           <div className="text-center md:text-left">
             <h2 className="text-xl font-semibold text-center">Marketing</h2>
             <p className="text-gray-600 font-medium mt-2 text-center">
@@ -57,16 +58,16 @@ const Home = () => {
             </p>
           </div>
           <img
-            src="https://images.freeimages.com/images/premium/previews/1227/12275386-development-graph.jpg"
+            src={marketing}
             alt="Development"
-            className="w-full md:w-1/2 rounded-lg shadow-lg"
+            className="w-full md:w-2/5  shadow-lg"
           />
         </div>
         <div className="flex flex-col md:flex-row items-center justify-center gap-8">
           <img
-            src="https://images.freeimages.com/images/premium/previews/1227/12275386-development-graph.jpg"
+            src={developments}
             alt="Development"
-            className="w-full md:w-1/2 rounded-lg shadow-lg"
+            className="w-full md:w-2/5  shadow-lg"
           />
           <div className="text-center md:text-left">
             <h2 className="text-xl font-semibold text-center">Development</h2>
@@ -80,7 +81,6 @@ const Home = () => {
             </p>
           </div>
         </div>
-
       </div>
     </>
   );
